fa6609437688c6ba18022b154c19a904ccabf2f3
sudalaiyandi sabarish kumar <sudalaiyandi@student.tce.edu>
2024-12-29 13:30:41 +0530

    Switch from bcrypt to bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const Post = require('../models/post');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const auth = require('../middleware/auth');
 const router = express.Router();
 
